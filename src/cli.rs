a95530bd3bb4f9768774cae4fa0f4a7d8bde9832
xtrellah <maxestrella565@gmail.com>
2026-04-30 20:50:01 +0100

    Refactor so load_commands logic is in cli

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,29 @@
-// cli.rs
+use crate::config;
+use crate::executor;
 use clap::Parser;
 
 #[derive(Parser)]
 pub struct Cli {
     pub command: String,
+}
+
+pub fn cli() {
+    load_commands();
+}
+
+fn load_commands() {
+    let cli: Cli = Cli::parse();
+
+    let config = config::load_config();
+
+    match config.commands.get(&cli.command) {
+        Some(path) => {
+            if let Err(e) = executor::run_script(path) {
+                eprintln!("Execution failed: {}", e);
+            }
+        }
+        None => {
+            eprintln!("Command not found");
+        }
+    }
 }
